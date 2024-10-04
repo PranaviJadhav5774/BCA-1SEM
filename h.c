227a367420f11a8b699a39a5d575aba3dc44cfac
PranaviJadhav5774 <jadhavpranavi5774@gmail.com>
2024-10-04 09:04:17 +0530

    Add files via upload

---
+++ h.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    //variable decalaration
+    int no2, no2, add, sub, mult, div, mod;
+    printf("enter integer number:");
+    scanf("%d",and no1)
+    scanf("enter integer number:");
+    scanf("%d"and no2)
+    add=no1=no2;
+    sub=no1-no2;
+    mult=no*no2;
+    div=no1/no2;
+    mod=no1%no2;
+    printf("\naddition is %d",add);
+    printf("nsubstration is %d",sub);
+    printf("multiplication is %d",mult);
+    printf("ndivision is %d" ,div);
+    printf("modulo division %d",mod);
+return0;  
+
+}
+ 
+
+
+ 
+     
+
